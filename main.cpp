926e7841362867e5da78dc94311ef0bbf75ee6e9
NGUYEN HUU DAT <127616184+datqg0@users.noreply.github.com>
2024-03-11 08:47:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ struct process {
   string process_name;
   double arrival_time;
   double duration;
+  double pridegree;
 };
 
 void display (vector<process>&process_box,vector<double>&gantt_chart_time,vector<string>&gantt_run_order,double&waitting_time) {
@@ -15,9 +16,11 @@ void display (vector<process>&process_box,vector<double>&gantt_chart_time,vector
     }
     cout<<endl;
     cout<<"thoi gian cho trung binh la : "<<waitting_time/(double)(process_box.size());
+    cout<<endl<<endl;
 }
 
-void firstcomefirstserve (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) {
+void FCFS (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) {
+  // O(n)
   if(process_box.size()==0) {
     return;
   }
@@ -35,11 +38,80 @@ void firstcomefirstserve (vector<process>&process_box,vector<string>&gantt_run_o
     waitting_time+=(now-process_box[i].arrival_time);
   }
   display (process_box,gantt_chart_time,gantt_run_order,waitting_time);
-  process_box.clear();
   gantt_chart_time.clear();
   gantt_run_order.clear();
 }
+class shorter {
+  public:
+    bool operator () (process&a,process&b) {
+        if(a.duration>b.duration) {
+          return true;
+        }
+        if(a.duration==b.duration) {
+          return a.arrival_time>b.arrival_time;
+        }
+        return false;
+    }
+};
+//non-preemptive
+void SJF1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+  if(process_box.size()==0) {
+    return;
+  }
 
+  //cout<<"run"<<endl;
+  int n=process_box.size();
+  int i=1;
+  int now=0;
+  priority_queue<process,vector<process>,shorter> pq;
+  pq.push(process_box[0]);
+  now=process_box[0].arrival_time;
+  while(i<n&&process_box[i].arrival_time<=now) {
+    pq.push(process_box[i]);
+    i++;
+  }
+  while(pq.empty()==false) {
+    process imple = pq.top();
+    pq.pop();
+    gantt_run_order.push_back(imple.process_name);
+    now+=imple.duration;
+    gantt_chart_time.push_back(now);
+    if(i<n) {
+      while(i<n&&process_box[i].arrival_time<=now) {
+        pq.push(process_box[i]);
+        i++;
+      }
+    }
+  }
+  display (process_box,gantt_chart_time,gantt_run_order,waitting_time);
+  gantt_chart_time.clear();
+  gantt_run_order.clear();
+}
+//preemptive
+void SJF2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+  if(process_box.size()==0) {
+    return;
+  }
+
+}
+void PRI1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+  if(process_box.size()==0) {
+    return;
+  }
+
+}
+void PRI2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+  if(process_box.size()==0) {
+    return;
+  }
+
+}
+void RR (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+  if(process_box.size()==0) {
+    return;
+  }
+
+}
 int main() {
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
@@ -57,5 +129,6 @@ int main() {
     process temp= {s,a,b};
     process_box.push_back(temp);
   }
-  firstcomefirstserve(process_box,gantt_run_order,gantt_chart_time,waitting_time);
+  FCFS(process_box,gantt_run_order,gantt_chart_time,waitting_time);
+  SJF1(process_box,gantt_run_order,gantt_chart_time,waitting_time);
 }
