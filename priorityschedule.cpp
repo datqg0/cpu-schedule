d5ba39458faac567ed1adbeb3b572f810e1cfd99
NGUYEN HUU DAT <127616184+datqg0@users.noreply.github.com>
2024-03-26 00:35:46 +0700

    Update priorityschedule.cpp

--- priorityschedule.cpp
+++ priorityschedule.cpp
@@ -7,18 +7,7 @@ struct process {
   double duration;
   double prioritydegree;
 };
-// write the result
-void display (vector<process>&process_box,vector<double>&gantt_chart_time,vector<string>&gantt_run_order,double&waitting_time) {
-    cout<<0<<" ";
-    for (double i=0;i<gantt_chart_time.size();i++) {
-      cout<<gantt_run_order[i]<<" "<<gantt_chart_time[i]<<" ";
-    }
-    cout<<endl;
-    cout<<"thoi gian cho "<<waitting_time<<endl;
-    cout<<"thoi gian cho trung binh la : "<<waitting_time/(double)(process_box.size());
-    cout<<endl<<endl;
-}
-//custom comparator for priority queue
+
 class priority {
   public:
     bool operator () (process&a,process&b) {
@@ -32,13 +21,87 @@ class priority {
     }
 };
 
-void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+void display (vector<process>&process_box,vector<double>&gantt_chart_time,vector<string>&gantt_run_order) {
+    //display gantt chart
+    map<string,double> waittable;
+    map<string,double> inputtime;
+    for (int i=0;i<process_box.size();i++) {
+      inputtime[process_box[i].process_name]=process_box[i].arrival_time;
+    }
+    double taketime=0;
+    double sum=0;
+    for (int i=0;i<gantt_chart_time.size();i++) {
+      if(i>0) {
+        taketime=gantt_chart_time[i-1];
+      }
+      waittable[gantt_run_order[i]]+=taketime-inputtime[gantt_run_order[i]];
+      inputtime[gantt_run_order[i]]=gantt_chart_time[i];
+    }
+    vector<pair<int,int>> range;
+    int now=0;
+    cout<<0;
+    for (int i=0;i<gantt_chart_time.size();i++) {
+      string s(gantt_run_order[i].size(),' ');
+      cout<<" "<<s<<" "<<gantt_chart_time[i];
+      int clone =gantt_chart_time[i];
+      int nus=0;
+      while(clone>0) {
+        clone/=10;
+        nus++;
+      }
+      double clone2 = gantt_chart_time[i];
+      bool check=false;
+      while(clone2!=(int)clone2) {
+        clone2*=10;
+        nus++;
+        check=true;
+      }
+      nus+=check;
+      range.push_back({now,now+1+s.size()+1+1});
+      now=now+1+s.size()+nus+1;
+    }
+    cout<<endl;
+    now=0;
+    for (int i=0;i<range.size();i++) {
+      int pos=(range[i].first+range[i].second)/2;
+      while(now<range[i].first) {
+        cout<<' ';
+        now++;
+      }
+      while(now<range[i].second) {
+        if(now==pos) {
+          cout<<gantt_run_order[i];
+          now+=gantt_run_order[i].size();
+        }
+        else if(now==range[i].first) {
+          cout<<'|';
+          now++;
+        }
+        else {
+          cout<<' ';
+          now++;
+        }
+       }
+       if(i==range.size()-1) {
+          cout<<'|';
+        }
+    }
+    cout<<endl;
+    for (int i=0;i<process_box.size();i++) {
+      cout<<"thoi gian cho cua "<<process_box[i].process_name<<" : "<<waittable[process_box[i].process_name]<<endl;
+      sum+=waittable[process_box[i].process_name];
+    }
+    cout<<"tong thoi gian cho "<<sum<<endl;
+    cout<<"thoi gian cho trung binh la : "<<sum/(double)(process_box.size());
+    cout<<endl<<endl;
+}
+void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time) { 
   if(process_box.size()==0) {
     return;
   }
   int n=process_box.size();
   int i=1;
-  int now=0;
+  double now=0;
   priority_queue<process,vector<process>,priority> pq;
   pq.push(process_box[0]);
   now=process_box[0].arrival_time;
@@ -66,15 +129,14 @@ void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
         current=temp;
       }
     }
-    int take=now;
+    double take=now;
     if(i<n) {
-      int av_time=process_box[i].arrival_time-now;
+      double av_time=process_box[i].arrival_time-now;
       if(av_time>=current.duration) {
         if(gantt_run_order.size()==0||gantt_run_order[gantt_run_order.size()-1]!=current.process_name) {
           gantt_run_order.push_back(current.process_name);
           now+=current.duration;
           gantt_chart_time.push_back(now);
-          waitting_time+=(take-current.arrival_time);
           current.arrival_time=-1;
         }
         else {
@@ -89,7 +151,6 @@ void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
           gantt_run_order.push_back(current.process_name);
           gantt_chart_time.push_back(now);
           current.duration-=av_time;
-          waitting_time+=(take-current.arrival_time);
           current.arrival_time=now; 
         }
         else {
@@ -105,7 +166,6 @@ void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
           gantt_run_order.push_back(current.process_name);
           now+=current.duration;
           gantt_chart_time.push_back(now);
-          waitting_time+=(take-current.arrival_time);
           current.arrival_time=-1;    
         }
         else {
@@ -119,21 +179,18 @@ void PS2 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
         i++;
     }
   }
-  display (process_box,gantt_chart_time,gantt_run_order,waitting_time);
+  display (process_box,gantt_chart_time,gantt_run_order);
   gantt_chart_time.clear();
   gantt_run_order.clear();
-  waitting_time=0;
 }
 
-void PS1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time,double&waitting_time) { 
+void PS1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<double>&gantt_chart_time) { 
   if(process_box.size()==0) {
     return;
   }
-
-  //cout<<"run"<<endl;
   int n=process_box.size();
   int i=1;
-  int now=0;
+  double now=0;
   priority_queue<process,vector<process>,priority> pq;
   pq.push(process_box[0]);
   now=process_box[0].arrival_time;
@@ -156,7 +213,6 @@ void PS1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
     gantt_run_order.push_back(current.process_name);
     now+=current.duration;
     gantt_chart_time.push_back(now);
-    waitting_time+=(take-current.arrival_time);
     if(i<n) {
       while(i<n&&process_box[i].arrival_time<=now) {
         pq.push(process_box[i]);
@@ -164,10 +220,9 @@ void PS1 (vector<process>&process_box,vector<string>&gantt_run_order,vector<doub
       }
     }
   }
-  display (process_box,gantt_chart_time,gantt_run_order,waitting_time);
+  display (process_box,gantt_chart_time,gantt_run_order);
   gantt_chart_time.clear();
   gantt_run_order.clear();
-  waitting_time=0;
 }
 
 int main() {
@@ -187,5 +242,8 @@ int main() {
     process temp= {s,a,b,p};
     process_box.push_back(temp);
   }
-  PS2(process_box,gantt_run_order,gantt_chart_time,waitting_time);
+  cout<<"Dieu phoi uu tien doc quyen :"<<endl;
+  PS1(process_box,gantt_run_order,gantt_chart_time);
+  cout<<"Dieu phoi uu tien doc quyen :"<<endl;
+  PS2(process_box,gantt_run_order,gantt_chart_time);
 }
